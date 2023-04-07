3c6915d602a70e5410836b2e1447e6eb3874f6f1
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-04-07 16:39:14 -0500

    Add ES6 arrow functions

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { DateTime } from './node_modules/luxon/src/luxon.js';
 import BookList from './modules/BookList.js';
 import navigation from './modules/navigation.js';
 
-function displayDate() {
+const displayDate = () => {
   const currentDate = DateTime.local().toLocaleString(DateTime.DATE_FULL);
   document.getElementById('date').textContent = currentDate;
-}
+};
 
 window.addEventListener('load', displayDate);
 
