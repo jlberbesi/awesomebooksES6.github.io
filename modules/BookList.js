c47c778e8720083922d057973b9be1890935b1f2
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-04-07 12:09:05 -0500

    Add modules

---
+++ modules/BookList.js
@@ -0,0 +1,55 @@
+import Book from './Book.js';
+
+class BookList {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.bookList = document.getElementById('book-list');
+    this.titleInput = document.getElementById('title');
+    this.authorInput = document.getElementById('author');
+    this.addBook = this.addBook.bind(this);
+    document.getElementById('book-form').addEventListener('submit', this.addBook);
+    this.displayBooks();
+  }
+
+  addBook(e) {
+    e.preventDefault();
+    const book = new Book(this.titleInput.value, this.authorInput.value, () => {
+      this.books = this.books.filter((b) => b !== book);
+      localStorage.setItem('books', JSON.stringify(this.books));
+      this.displayBooks();
+    });
+    this.books.push(book);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+    this.titleInput.value = '';
+    this.authorInput.value = '';
+    const message = document.createElement('p');
+    message.textContent = 'A new book was added';
+    message.classList.add('message');
+    document.getElementById('book-form').appendChild(message);
+    setTimeout(() => message.remove(), 3000);
+  }
+
+  displayBooks() {
+    this.bookList.innerHTML = '';
+    this.books.forEach((book) => {
+      const li = document.createElement('li');
+      li.classList.add('book');
+      const title = document.createElement('span');
+      title.textContent = book.title;
+      li.appendChild(title);
+      const author = document.createElement('span');
+      author.textContent = ` by ${book.author}`;
+      li.appendChild(author);
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      li.appendChild(removeBtn);
+      removeBtn.addEventListener('click', () => {
+        book.remove();
+      });
+      this.bookList.appendChild(li);
+    });
+  }
+}
+
+export default BookList;
